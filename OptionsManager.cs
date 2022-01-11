f79b07c8e4ae540c558bf43ba6c48f1b9cd307c9
parshev <s.parshev19@gmail.com>
2022-01-11 09:01:04 +0200

    Homework added

---
+++ OptionsManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GitExercise
+{
+    public static class OptionsManager
+    {
+        public static string[] OptionsList = {
+            "a - Add"
+        };
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+       
+    }
+}
