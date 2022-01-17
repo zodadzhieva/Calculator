cd0f0d815bfd4a89b03e18dfa2af38d5e0f386d2
zodadzhieva <zornica_odadjieva@abv.bg>
2022-01-17 17:41:54 +0200

    Second commit

--- OptionsManager.cs
+++ OptionsManager.cs
@@ -5,14 +5,26 @@ namespace GitExercise
     public static class OptionsManager
     {
         public static string[] OptionsList = {
-            "a - Add"
+            "a - Add",
+            "s-Subtract",
+            "m-Multiply",
+
         };
 
         public static void Add(double a, double b)
         {
             Console.WriteLine($"{a} + {b} = {a + b}");
+            
         }
 
-       
+        public static void Subtract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} * {b} = {a * b}");
+        }
     }
 }
